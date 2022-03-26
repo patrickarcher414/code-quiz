646d6cec7fb1ecd919f6bfe71333e8df0938129c
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-26 01:14:09 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// global vars
+var questions = document.querySelector('#questions')
+
+
+// // functions
+
+function createQuestion() {
+    questions.innerHTML = "yolo"
+}
+
+function startQuiz() {
+    // code
+}
+
+// // call start quiz
+
+
+
