eac1f31f384c7d83670f9ddcffd7ab0796b01322
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-27 12:38:57 -0500

    add timer feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,41 @@
 
 // global vars
-var questions = document.querySelector('#questions')
-
+var questionsEl = document.querySelector('#questions')
+var timerEl = document.querySelector('#timer')
+var scoreEl = document.querySelector('#score')
+var startingTime = 10
+var newTime = startingTime
+var intervalId
 
 // // functions
 
+// create question element, append to page
 function createQuestion() {
-    questions.innerHTML = "yolo"
+    // code
 }
 
+function gameOver() {
+    
+}
+
+function updateTimer() {
+    intervalId = setInterval(function() {
+        newTime--;
+        timerEl.innerHTML = 'Timer: ' + newTime;
+        if (newTime === 0) {
+            clearInterval(intervalId)
+            gameOver()
+        }
+    }, 1000)
+}
+
+
 function startQuiz() {
-    // code
+    updateTimer();
+    
 }
 
 // // call start quiz
 
-
+startQuiz();
 
