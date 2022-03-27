6c655ba08de9dccbd6a70d250f0bbec8499ad69e
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-27 16:57:39 -0500

    checkanswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,55 @@ var startingTime = 10
 var newTime = startingTime
 var intervalId
 var score = 0
+var questionEl = document.querySelector('#questions')
+var questionIndex = 0
+var userChoice
+var questionNum
+
+// create an array of objects to get questions from
+var questions = [
+    {question: "What is ___",
+    answer: "A: __",
+    choices: ["A: __", "B: jfgdg", "C:jkgdhkjg", "D: jkhgjhgs"]
+    },
+    {question: "How do you ___",
+    answer: "C: ___",
+    choices: ["A: diushsjh", "B: kjdhfkjghg", "C: ___", "D: kjsdhgj"]
+    }
+]
 
 // // functions
 
 // create question element, append to page
-function createQuestion() {
-    // code
+function createQuestion(questionNum) {
+    questionEl.innerText = questions[questionNum].question
+    for (var i=0; i < questions[questionNum].choices.length; i++) {
+        var button = document.createElement('button')
+        button.setAttribute('class', 'btn btn-secondary m-2')
+        questionEl.appendChild(button)
+        button.innerText = questions[questionNum].choices[i]
+    }
+}
+
+
+function questionAnswer() {
+    questionsEl.addEventListener('click', function(event) {
+        if (event.target.getAttribute('class') === 'btn btn-secondary m-2') {
+        userChoice = event.target.innerHTML
+        handleQuestionAnswer(questionIndex);
+        questionIndex++
+        }
+    });
+}
+
+function handleQuestionAnswer(questionNum) {
+    var correctAnswer = questions[questionNum].answer;
+    if (userChoice === correctAnswer) {
+        score++
+        scoreEl.innerText = score
+    } else {
+        newTime -= 5;
+    }
 }
 
 function gameOver() {
@@ -40,7 +83,9 @@ function updateTimer() {
 
 function startQuiz() {
     updateTimer();
-    
+    createQuestion(0);
+    questionAnswer();
+
 }
 
 // // call start quiz
